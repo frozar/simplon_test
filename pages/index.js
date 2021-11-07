3800940027eaa0da4486e9596c9b96ccc513bade
frozar <fabien.rozar@gmail.com>
2021-11-07 11:30:58 +0400

    Externalise administration modal.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Head from "next/head";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
 import LogoutIcon from "@mui/icons-material/Logout";
 
 import Timeline, {
@@ -17,16 +15,7 @@ import moment from "moment";
 // will not be styled
 import "react-calendar-timeline/lib/Timeline.css";
 
-const styleModal = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "background.paper",
-  borderRadius: "20px",
-  boxShadow: 24,
-  p: 2,
-};
+import AdministrationPanel from "../components/AdministrationPanel";
 
 const styleLogoutButton = {
   position: "absolute",
@@ -122,46 +111,7 @@ export default function Home() {
           <DateHeader />
         </TimelineHeaders>
       </Timeline>
-      <Modal
-        open={openModal}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={styleModal}>
-          <Grid container justifyContent="center">
-            <Grid item>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Panneau d&apos;administration
-              </Typography>
-            </Grid>
-            <Grid
-              container
-              item
-              justifyContent="space-evenly"
-              style={{ marginTop: "15px" }}
-            >
-              <Grid item>
-                <Button
-                  variant="contained"
-                  onClick={() => alert("utilisateur")}
-                >
-                  <Typography id="admin-utilisateur-button" variant="button">
-                    Gérer les utilisateurs
-                  </Typography>
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={() => alert("machine")}>
-                  <Typography id="admin-machine-button" variant="button">
-                    Gérer les machines
-                  </Typography>
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Box>
-      </Modal>
+      <AdministrationPanel openModal={openModal} handleClose={handleClose} />
     </>
   );
 }
