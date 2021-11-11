ed1c5985b547fff25e34697256e6baa09a9f15da
frozar <fabien.rozar@gmail.com>
2021-11-11 19:52:37 +0400

    Add constraint of booking create and edition.

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,5 @@
 export const SUCCESS = 0;
 export const ALREADY_EXIST = 1;
+export const CHRONOLOGICAL_ERROR = 2;
+export const USER_INTERSECTION_ERROR = 3;
+export const COMPUTER_INTERSECTION_ERROR = 4;
