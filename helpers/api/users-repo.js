b2bac863b5bcc0a3e101f06bf22a593b0f950f3f
frozar <fabien.rozar@gmail.com>
2021-11-12 13:30:59 +0400

    Delete user repo.

--- helpers/api/users-repo.js
+++
@@ -1,8 +0,0 @@
-// users in JSON file for simplicity, store in a db for production applications
-// let users = require('data/users.json');
-let users = [];
-
-export const usersRepo = {
-  getAll: () => users,
-  find: (x) => users.find(x),
-};
